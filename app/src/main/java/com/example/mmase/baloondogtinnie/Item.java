8c253aaf88c4955e30abed2288328019f81dbc01
mmase <machi0545@hotmail.com>
2016-01-18 13:47:24 +0900

    Add three statuses of tinnie

--- app/src/main/java/com/example/mmase/baloondogtinnie/Item.java
+++ app/src/main/java/com/example/mmase/baloondogtinnie/Item.java
@@ -78,6 +78,10 @@ public abstract class Item {
     public void setYBoarder(int YBoarder) {
         mYBoarder = YBoarder;
     }
+
+    protected void setRid(int rid) {
+        mRid = rid;
+    }
 }
 
 
